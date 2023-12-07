9e2a54f3522e5c5a6caa0f4fa770769c729a2092
ThomasRemmer <remmert765@gmail.com>
2023-12-07 15:15:16 +0000

    completed set of of initial pages/layout

--- app/character-sheet/page.js
+++ app/character-sheet/page.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Nav from "../components/nav";
-
-export default function Home() {
+import Nav from "../../components/nav";
+export default function CharacterSheet() {
   return (
     <>
       <main className="flex min-h-screen flex-col justify-between p-24">
-        <header className="flex flex-col  min-h-screen text-6xl text-center "></header>
+        <header className="flex flex-col  min-h-screen text-6xl text-center ">
+          <h1>Character Sheet</h1>
+        </header>
 
         <footer className="flex flex-col items-center justify-center w-full h-24 border-t"></footer>
       </main>
--- components/nav.jsx
+++ components/nav.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <h1 className="text-4xl font-bold text-center text-gray-800">
-        DND 5e Character Creator
-      </h1>
-      <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700">
-        Items List
-      </button>
-      <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700">
-        Monster List
-      </button>
-      <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700">
-        Spells List
-      </button>
-    </div>
+    <nav className="fixed top-0 left-0 right-0 z-10 flex items-center justify-around h-16 bg-gray-800 text-white">
+      <Link href="/">
+        <h1 className="text-4xl font-bold text-center text-red-400">
+          DND 5e Character Creator
+        </h1>
+      </Link>
+      <div>
+        <Link href="/character-sheet">
+          <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700">
+            Characters
+          </button>
+        </Link>
+        <button
+          className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700"
+          href
+        >
+          Campaigns
+        </button>
+        <button className="px-4 py-2 font-bold text-white bg-gray-800 rounded hover:bg-gray-700">
+          Profile
+        </button>
+      </div>
+    </nav>
   );
 }
